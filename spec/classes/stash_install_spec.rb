1845a72c4c937d68089fced655d03b45d44045b3
sebastian cole <sebastian.cole@mi9.com.au>
2014-05-01 11:12:16 +1000

    testing defaults for installation

--- spec/classes/stash_install_spec.rb
+++ spec/classes/stash_install_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper.rb'
 
 describe 'stash' do
-  context 'default params' do
-    it do
-      should contain_class('stash::install')
-      should contain_class('stash::config')
-      should contain_class('stash::service')
-    end
+describe 'stash::install' do
 
+  it 'should install, but not upgrade, git' do
+    should contain_package('git').with_ensure('installed')
   end
 
-  
+  context 'default params' do
+    it { should contain_group('stash') }
+    it { should contain_user('stash') }
+    it 'should deploy stash 2.12.0 from tar.gz' do
+      should contain_deploy__file("atlassian-stash-2.12.0.tar.gz")
+    end
+    it 'should manage the stash home directory' do
+      should contain_file('/home/stash').with({
+        'ensure' => 'directory',
+        'owner' => 'stash',
+        'group' => 'stash'
+        })
+    end
+  end
+
+end
 end
