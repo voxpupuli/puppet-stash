f0ff21730b9bf4cf06ec54f5e09de8d551d9ffdc
Merritt Krakowitzer <merritt@krakowitzer.com>
2014-09-14 23:22:22 +0200

    hardcode stash version in spec test fix tab spaces

--- spec/classes/stash_config_spec.rb
+++ spec/classes/stash_config_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper.rb'
 
 describe 'stash' do
 describe 'stash::config' do
-	context 'default params' do
-		it { should contain_file('/opt/stash/atlassian-stash-3.2.4/bin/setenv.sh')}
-		it { should contain_file('/opt/stash/atlassian-stash-3.2.4/bin/user.sh')}
-		it { should contain_file('/opt/stash/atlassian-stash-3.2.4/conf/server.xml')}
-		it { should contain_file('/home/stash/shared/stash-config.properties')}
-	end
+  context 'default params' do
+    let(:params) {{
+      :version => '3.2.4',
+    }}
+    it { should contain_file('/opt/stash/atlassian-stash-3.2.4/bin/setenv.sh')}
+    it { should contain_file('/opt/stash/atlassian-stash-3.2.4/bin/user.sh')}
+    it { should contain_file('/opt/stash/atlassian-stash-3.2.4/conf/server.xml')}
+    it { should contain_file('/home/stash/shared/stash-config.properties')}
+  end
 end
 end
