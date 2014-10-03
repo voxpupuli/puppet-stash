5870362345e7c01b1383952af221fd0371f20f4d
Merritt Krakowitzer <mkrakowitzer@fnb.co.za>
2014-10-03 17:10:29 +0200

    Add test for proxy

--- spec/classes/stash_config_spec.rb
+++ spec/classes/stash_config_spec.rb
@@ -5,10 +5,15 @@ describe 'stash::config' do
   context 'default params' do
     let(:params) {{
       :version => '3.2.4',
+      :proxy   => {
+        'scheme'    => 'https',
+        'proxyName' => 'stash.example.co.za',
+        'proxyPort' => '443',
+      },
     }}
     it { should contain_file('/opt/stash/atlassian-stash-3.2.4/bin/setenv.sh')}
     it { should contain_file('/opt/stash/atlassian-stash-3.2.4/bin/user.sh')}
-    it { should contain_file('/opt/stash/atlassian-stash-3.2.4/conf/server.xml')}
+    it { should contain_file('/opt/stash/atlassian-stash-3.2.4/conf/server.xml').with_content(/stash\.example\.co\.za/)}
     it { should contain_file('/home/stash/shared/stash-config.properties')}
   end
 end
