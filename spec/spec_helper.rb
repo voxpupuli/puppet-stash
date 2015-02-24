c6042a45849f53c8ff7826e08757572a0c298c63
Merritt Krakowitzer <merritt@krakowitzer.com>
2015-02-24 23:23:11 +0200

    Add smoke tests, add facts to pass strictmode=yes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 RSpec.configure do |c|
   c.default_facts = {
-    :osfamily => 'Debian',
+    :osfamily           => 'Debian',
+    :augeasversion      => '1.0.0',
+    :staging_http_get   => 'curl',
+    :path               => '/usr/local/bin:/usr/bin:/bin',
+    :stash_version      => '5.5.6',
+    :puppetversion      => '3.7.4',
   }
 end
