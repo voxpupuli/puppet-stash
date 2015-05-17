a205647a406d2dd9958d83dae30c36868b50fb30
Stephen Schmidt <flyers20032002@gmail.com>
2015-05-17 17:12:11 -0400

    Add unit tests for user and group management feature.
      - Add rspec-puppet unit tests to stash_install_deploy_spec and
        stash_install_staging_spec.
    
    Correct test cases for user and group management feature.
    
      - The tests for the lack of user and group within the catalog was
        not properly being done when the group_manage and user_manage were
        set to false.
      - Add tests to satisfy the above requirements.

--- spec/classes/stash_install_staging_spec.rb
+++ spec/classes/stash_install_staging_spec.rb
@@ -32,6 +32,78 @@ describe 'stash::install' do
           end
         end
       
+        context 'when managing the user and group outside the module' do
+          let(:params) {{
+            :user_manage  => 'false',
+            :group_manage => 'false',
+          }}
+          context 'when no user or group are specified' do
+            it do
+              should_not contain_user('stash').with({
+                'comment'          => 'Stash daemon account',
+                'shell'            => '/bin/bash',
+                'home'             => '/test/my/dir',
+                'password'         => '*',
+                'password_min_age' => '0',
+                'password_max_age' => '99999',
+                'managehome'       => true,
+                'uid'              => '1234',
+                'gid'              => '5678' 
+              })
+            end 
+            it do
+              should_not contain_group('stash').with({
+                'ensure' => 'present',
+                'gid'    => '5678' 
+              })
+            end
+          end
+          context 'when a user and group is specified' do
+            let(:params) {{
+              :user  => 'mystashuser',
+              :group => 'mystashgroup'
+            }}
+            it do
+              should_not contain_user('mystashuser').with({
+                'comment'          => 'Stash daemon account',
+                'shell'            => '/bin/bash',
+                'home'             => '/test/my/dir',
+                'password'         => '*',
+                'password_min_age' => '0',
+                'password_max_age' => '99999',
+                'managehome'       => true,
+                'uid'              => '1234',
+                'gid'              => '5678' 
+              })
+            end 
+            it do
+              should_not contain_group('mystashgroup').with({
+                'ensure' => 'present',
+                'gid'    => '5678'
+              })
+            end
+          end
+        end
+
+        #context 'when managing the user and group inside the module' do
+        #  let(:params) {{
+        #    :user_manage  => 'true',
+        #    :group_manage => 'true'
+        #  }}
+        #  context 'when no user or group are specified' do
+        #    it { should contain_user('stash') }
+        #    it { should contain_group('stash') }
+        #  end
+        #  context 'when a user and group is specified' do
+        #    let(:params) {{
+        #      :user  => 'mystashuser',
+        #      :group => 'mystashgroup'
+        #    }}
+        #    it { should contain_user('mystashuser') }
+        #    it { should contain_group('mystashgroup') }
+        #  end
+        #end
+
         context 'overwriting params' do
           let(:params) {{
             :version     => '3.7.0',
