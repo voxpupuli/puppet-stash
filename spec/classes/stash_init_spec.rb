659fde613d007f74021ac4b80be3e2945939d83f
Dennis Hoppe <github@debian-solutions.de>
2016-05-22 11:14:44 +0200

    Fix several RSpec issues

--- spec/classes/stash_init_spec.rb
+++ spec/classes/stash_init_spec.rb
@@ -20,12 +20,12 @@ describe 'stash' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_class('stash') }
           it { is_expected.to contain_class('stash::params') }
-          it { is_expected.to contain_anchor('stash::start').that_comes_before('stash::install') }
-          it { is_expected.to contain_class('stash::install').that_comes_before('stash::config') }
+          it { is_expected.to contain_anchor('stash::start').that_comes_before('Class[stash::install]') }
+          it { is_expected.to contain_class('stash::install').that_comes_before('Class[stash::config]') }
           it { is_expected.to contain_class('stash::config') }
           it { is_expected.to contain_class('stash::backup') }
-          it { is_expected.to contain_class('stash::service').that_subscribes_to('stash::config') }
-          it { is_expected.to contain_anchor('stash::end').that_requires('stash::service') }
+          it { is_expected.to contain_class('stash::service').that_subscribes_to('Class[stash::config]') }
+          it { is_expected.to contain_anchor('stash::end').that_requires('Class[stash::service]') }
           it { is_expected.to contain_class('archive') }
           it { is_expected.to contain_service('stash') }
         end
