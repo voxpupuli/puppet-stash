14c09c477c6cab86925fd779fa459d7e63b56df5
Tim Meusel <tim@bastelfreak.de>
2016-08-08 10:52:52 +0200

    manage spec_helper.rb again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,22 +3,17 @@ require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
-  c.before do
-    # avoid "Only root can execute commands as other users"
-    Puppet.features.stubs(root?: true)
-  end
-end
-
-RSpec.configure do |c|
-  c.default_facts = {
-    stash_version: '3.9.2',
-    staging_http_get: 'curl',
-    os_maj_version: '6',
-    puppetversion: '3.7.4',
+  default_facts = {
+    puppetversion: Puppet.version,
+    facterversion: Facter.version
   }
+  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
+  default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
+  c.default_facts = default_facts
 end
 
 STASH_VERSION = '3.11.4'.freeze
 BACKUP_VERSION = '1.9.1'.freeze
 BITBUCKET_VERSION = '4.0.2'.freeze
 BITBUCKET_BACKUP_VERSION = '2.0.0'.freeze
+# vim: syntax=ruby
